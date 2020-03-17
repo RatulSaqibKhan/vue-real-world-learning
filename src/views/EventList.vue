ad1b5d93313ae3c9b6a670bb9050716643e22174
Md. Nazmus Saqib Khan <ratulkhan.jhenidah@gmail.com>
2020-03-17 22:08:16 +0600

    Initial Commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 @click="handler(arg, event)"></h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'Single Quote'
+    }
+  }
+}
+</script>
+
+<style></style>
