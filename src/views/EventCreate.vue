c5b4b076fbe26b952e28b36a017c873be10a584f
Saqibkhan <saqib@skylarksoft.com>
2020-05-10 12:32:57 +0600

    simplifying mapState v1

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h1>Create an Event, {{ userName }}</h1>
-    <p>Created by, {{ userId }}</p>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>Created by, {{ user.id }}</p>
     <h4>Categories</h4>
     <ul v-for="(category, index) in categories" :key="index">
       <li>{{ category }}</li>
@@ -15,9 +15,8 @@ import { mapState } from 'vuex'
 export default {
   name: 'EventCreate',
   computed: mapState({
-    userName: state => state.user.name,
-    userId: state => state.user.id,
-    categories: state => state.categories
+    user: 'user',
+    categories: 'categories'
   }),
   data() {
     return {
