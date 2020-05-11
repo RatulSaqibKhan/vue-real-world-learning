6c34a24e6ddc491ca8c52c3e85e6caf50f70e590
Saqibkhan <saqib@skylarksoft.com>
2020-05-11 21:02:17 +0600

    Vuex mutations and actions learnt and create event using this concept

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,30 +1,143 @@
 <template>
   <div>
-    <h1>Create an Event, {{ user.name }}</h1>
-    <p>Created by, {{ user.id }}</p>
-    <h4>There are {{ categoryLength }} categories:</h4>
-    <ul v-for="(category, index) in categories" :key="index">
-      <li>{{ category }}</li>
-    </ul>
-    <p>Event {{ getEventById(4) }}</p>
+    <h1>Create an Event</h1>
+    <br />
+    <form @submit.prevent="createEvent">
+      <div class="field">
+        <label>Select a category</label>
+        <select v-model="event.category">
+          <option v-for="category in categories" :key="category">{{
+            category
+          }}</option>
+        </select>
+      </div>
+
+      <h3>Name &amp; describe event</h3>
+      <div class="field">
+        <label>Title</label>
+        <input
+          type="text"
+          v-model="event.title"
+          placeholder="Add an event title"
+        />
+      </div>
+
+      <div class="field">
+        <label>Description</label>
+        <input
+          type="text"
+          v-model="event.description"
+          placeholder="Add a description"
+        />
+      </div>
+
+      <h3>Where is your event?</h3>
+
+      <div class="field">
+        <label>Location</label>
+        <input
+          type="text"
+          v-model="event.location"
+          placeholder="Add a location"
+        />
+      </div>
+
+      <h3>When is your event?</h3>
+
+      <div class="field">
+        <label>Date</label>
+        <datepicker v-model="event.date" placeholder="Select a date" />
+      </div>
+
+      <div class="field">
+        <label>Select a time</label>
+        <select v-model="event.time">
+          <option v-for="time in times" :key="time">{{ time }}</option>
+        </select>
+      </div>
+
+      <input type="submit" class="button --fill-gradient" value="Submit" />
+    </form>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
+import Datepicker from 'vuejs-datepicker'
 
 export default {
   name: 'EventCreate',
-  computed: {
-    ...mapState(['user', 'categories']),
-    ...mapGetters(['categoryLength', 'getEventById'])
+  components: {
+    Datepicker
   },
   data() {
+    const times = []
+    for (let i = 1; i <= 24; i++) {
+      times.push(i + ':00')
+    }
     return {
-      key: ''
+      event: this.createFreshEvent(),
+      times,
+      categories: this.$store.state.categories
+    }
+  },
+
+  methods: {
+    createEvent() {
+      this.$store
+        .dispatch('createEvent', this.event)
+        .then(() => {
+          this.$router.push({
+            name: 'EventShow',
+            params: { id: this.event.id }
+          })
+          this.event = this.createFreshEvent
+        })
+        .catch(() => {
+          alert('Something went wrong creating the event!!')
+        })
+    },
+
+    createFreshEvent() {
+      const user = this.$store.state.user
+      const id = Math.floor(Math.random() * 10000000)
+
+      return {
+        id: id,
+        user: user,
+        category: '',
+        organizer: user,
+        title: '',
+        description: '',
+        location: '',
+        date: '',
+        time: '',
+        attendees: []
+      }
     }
   }
 }
 </script>
 
-<style></style>
+<style scoped>
+.button {
+  padding: 10px 23px;
+  border-radius: 14px;
+  font-weight: 700;
+  border: none;
+  cursor: pointer;
+}
+
+.button:hover {
+  box-shadow: 0 0 17px #16c0b0;
+  border: 1px solid #84cf6a;
+}
+
+.--fill-gradient {
+  background: linear-gradient(to bottom, #16c0b0, #84cf6a);
+  color: #fff;
+}
+
+.field {
+  margin-bottom: 24px;
+}
+</style>
