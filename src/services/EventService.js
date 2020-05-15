98c73dc03b5060f9021a1e34982f680b6f0f23a2
Saqibkhan <saqib@skylarksoft.com>
2020-05-15 14:37:53 +0600

    Implementing progressbar using axios inetrceptors

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,12 +1,24 @@
 import axios from 'axios'
+import NProgress from 'nprogress'
 
 const apiClient = axios.create({
   baseURL: 'http://localhost:3000',
-  withCredentials: false,
+  withCredentials: false, // This is default
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
   }
+})
+
+//Learning Axios interceptor
+apiClient.interceptors.request.use(config => {
+  NProgress.start()
+  return config
+})
+
+apiClient.interceptors.response.use(response => {
+  NProgress.done()
+  return response
 })
 
 export default {
